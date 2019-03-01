5256fdc0f2ca2fe2f51f6b45cab7df2a6ac4c632
Shimul <shimuldn@gmail.com>
2019-03-01 22:01:53 +0530

    Before Category

---
+++ Todoey/Controllers/CategoryViewController.swift
@@ -0,0 +1,32 @@
+//
+//  CategoryViewController.swift
+//  Todoey
+//
+//  Created by hydra on 01/03/19.
+//  Copyright © 2019 Shimul. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    //MARK: - TableView Datasource Methods
+    
+    //MARK: - TableView Delegate Methods
+    
+    //MARK: - Add New Categories
+
+    @IBAction func addButtionPressed(_ sender: UIBarButtonItem) {
+        
+    }
+    
+    
+    //MARK: - Data Manipulation Methods
+    
+}
