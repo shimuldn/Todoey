8638c70840d646b3fd646b282eaa37b747b55655
Shimul <shimuldn@gmail.com>
2019-03-02 19:36:25 +0530

    Persisting data using Core data Category setup completed

--- Todoey/Controllers/CategoryViewController.swift
+++ Todoey/Controllers/CategoryViewController.swift
@@ -10,23 +10,80 @@ import UIKit
 import CoreData
 
 class CategoryViewController: UITableViewController {
+    var categories = [Category]()
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
         
+        loadCategory()
     }
     
     //MARK: - TableView Datasource Methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categories.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CatagoryCell", for: indexPath)
+        
+        cell.textLabel?.text = categories[indexPath.row].name
+        
+        return cell
+    }
+    
     
     //MARK: - TableView Delegate Methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "goToItems", sender: self)
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destinationVC = segue.destination as! TodoListViewController
+        
+        if let indexPath = tableView.indexPathForSelectedRow {
+            destinationVC.selectedCategory = categories[indexPath.row]
+        }
+    }
+    
     
     //MARK: - Add New Categories
 
     @IBAction func addButtionPressed(_ sender: UIBarButtonItem) {
-        
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add new Category", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Category", style: .default) { (action) in
+            let newCategory = Category(context: self.context)
+            newCategory.name = textField.text
+            
+            self.categories.append(newCategory)
+            self.saveCategory()
+        }
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create New Category"
+            textField = alertTextField
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        print("Added Categoty")
     }
     
     
     //MARK: - Data Manipulation Methods
+    func saveCategory() {
+        do {
+            try context.save()
+        } catch {
+            print("Error encoing item array \(error)")
+        }
+        tableView.reloadData()
+    }
     
+    func loadCategory(with request: NSFetchRequest<Category> = Category.fetchRequest()) {
+        do {
+            categories = try context.fetch(request)
+        } catch {
+            print("Error fatch request \(error)")
+        }
+        tableView.reloadData()
+    }
 }
